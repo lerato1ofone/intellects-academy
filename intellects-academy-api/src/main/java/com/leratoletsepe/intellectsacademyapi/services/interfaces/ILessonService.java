458aa716c1f03db8775e64c3a726afae75c473ca
lerato1ofone <letsepe100@gmail.com>
2021-04-09 18:56:15 +0200

    Added service to delete lesson

--- intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/interfaces/ILessonService.java
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/interfaces/ILessonService.java
@@ -1,9 +1,12 @@
 package com.leratoletsepe.intellectsacademyapi.services.interfaces;
 
 import com.leratoletsepe.intellectsacademyapi.exceptions.IaBadRequestException;
+import com.leratoletsepe.intellectsacademyapi.exceptions.IaNotFoundException;
 
 import java.time.LocalDate;
 
 public interface ILessonService {
     void addLesson(Integer courseId, String title, LocalDate date, String content) throws IaBadRequestException;
+
+    void deleteLesson(Integer userId, Integer lessonId) throws IaNotFoundException;
 }
