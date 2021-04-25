ec3ce6392e7a2e8f66909e6b5e3cc89d761de353
lerato1ofone <letsepe100@gmail.com>
2021-04-25 20:04:18 +0200

    Added lesson service class

--- intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/LessonService.java
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/LessonService.java
@@ -1,4 +1,16 @@
 package com.leratoletsepe.intellectsacademyapi.services;
 
-public class LessonService {
+import com.leratoletsepe.intellectsacademyapi.exceptions.IaBadRequestException;
+import com.leratoletsepe.intellectsacademyapi.services.interfaces.ILessonService;
+import org.springframework.stereotype.Service;
+
+import java.sql.Date;
+
+@Service
+public class LessonService implements ILessonService {
+
+    @Override
+    public void AddLesson(Integer courseId, String title, Date date, String content) throws IaBadRequestException {
+
+    }
 }
