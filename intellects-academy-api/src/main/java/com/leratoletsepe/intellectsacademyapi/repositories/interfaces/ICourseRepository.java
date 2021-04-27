75ce8d6d099b7a5163c1cfb900a689b45a148be1
lerato1ofone <letsepe100@gmail.com>
2021-04-27 15:58:16 +0200

    Updated course repository to add find by id

--- intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/repositories/interfaces/ICourseRepository.java
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/repositories/interfaces/ICourseRepository.java
@@ -1,10 +1,14 @@
 package com.leratoletsepe.intellectsacademyapi.repositories.interfaces;
 
 import com.leratoletsepe.intellectsacademyapi.exceptions.IaBadRequestException;
+import com.leratoletsepe.intellectsacademyapi.exceptions.IaNotFoundException;
+import com.leratoletsepe.intellectsacademyapi.models.Course;
 import com.leratoletsepe.intellectsacademyapi.models.Lesson;
 
 import java.util.List;
 
 public interface ICourseRepository {
     void create(Integer userId, String title, String description, List<Lesson> lessons) throws IaBadRequestException;
+
+    Course findById(Integer courseId) throws IaNotFoundException;
 }
