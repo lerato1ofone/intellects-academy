cb316d630b2a6a2817ee3fd6efaa7f423a9ae8bf
lerato1ofone <letsepe100@gmail.com>
2021-04-28 13:01:02 +0200

    Add function to find all lessons in lesson repository interface

--- intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/repositories/interfaces/ILessonRepository.java
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/repositories/interfaces/ILessonRepository.java
@@ -1,9 +1,14 @@
 package com.leratoletsepe.intellectsacademyapi.repositories.interfaces;
 
 import com.leratoletsepe.intellectsacademyapi.exceptions.IaBadRequestException;
+import com.leratoletsepe.intellectsacademyapi.exceptions.IaNotFoundException;
+import com.leratoletsepe.intellectsacademyapi.models.Lesson;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface ILessonRepository {
     void create(Integer courseId, String title, LocalDate date, String content) throws IaBadRequestException;
+
+    List<Lesson> findAll(Integer courseId) throws IaNotFoundException;
 }
