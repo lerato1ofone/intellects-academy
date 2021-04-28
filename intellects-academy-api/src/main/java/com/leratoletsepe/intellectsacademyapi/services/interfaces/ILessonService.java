55610fc4432a02a9f36e1ded7d13ae4af5a41588
lerato1ofone <letsepe100@gmail.com>
2021-04-28 13:01:01 +0200

    Update date to localdate in lesson service interface

--- intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/interfaces/ILessonService.java
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/interfaces/ILessonService.java
@@ -3,7 +3,8 @@ package com.leratoletsepe.intellectsacademyapi.services.interfaces;
 import com.leratoletsepe.intellectsacademyapi.exceptions.IaBadRequestException;
 
 import java.sql.Date;
+import java.time.LocalDate;
 
 public interface ILessonService {
-    void AddLesson(Integer courseId, String title, Date date, String content) throws IaBadRequestException;
+    void AddLesson(Integer courseId, String title, LocalDate date, String content) throws IaBadRequestException;
 }
