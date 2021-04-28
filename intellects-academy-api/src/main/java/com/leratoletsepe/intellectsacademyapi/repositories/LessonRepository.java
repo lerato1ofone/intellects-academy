b8fbc9f3bc36914d0dbc9bd359047210a8c020d5
lerato1ofone <letsepe100@gmail.com>
2021-04-28 12:15:02 +0200

    Autowire jdbcTemplate and implemented lesson interface repository

--- intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/repositories/LessonRepository.java
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/repositories/LessonRepository.java
@@ -1,7 +1,20 @@
 package com.leratoletsepe.intellectsacademyapi.repositories;
 
+import com.leratoletsepe.intellectsacademyapi.repositories.interfaces.ILessonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
+
 @Repository
-public class LessonRepository {
+public class LessonRepository implements ILessonRepository {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void create(Integer courseId, String title, Date date, String content) {
+
+    }
 }
