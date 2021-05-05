de00d080987d602e9582cd9f6507110b2170731b
lerato1ofone <letsepe100@gmail.com>
2021-05-05 16:12:14 +0200

    Updated lesson service interface

--- intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/interfaces/ILessonService.java
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/interfaces/ILessonService.java
@@ -2,9 +2,8 @@ package com.leratoletsepe.intellectsacademyapi.services.interfaces;
 
 import com.leratoletsepe.intellectsacademyapi.exceptions.IaBadRequestException;
 
-import java.sql.Date;
 import java.time.LocalDate;
 
 public interface ILessonService {
-    void AddLesson(Integer courseId, String title, LocalDate date, String content) throws IaBadRequestException;
+    void addLesson(Integer courseId, String title, LocalDate date, String content) throws IaBadRequestException;
 }
