051484f710d6ff5137f3c80bb5eb52dab6339a22
lerato1ofone <letsepe100@gmail.com>
2021-05-06 16:12:14 +0200

    Implemented attach lesson to course after adding it

--- intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/LessonService.java
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/LessonService.java
@@ -1,22 +1,45 @@
 package com.leratoletsepe.intellectsacademyapi.services;
 
 import com.leratoletsepe.intellectsacademyapi.exceptions.IaBadRequestException;
+import com.leratoletsepe.intellectsacademyapi.models.Course;
+import com.leratoletsepe.intellectsacademyapi.models.Lesson;
+import com.leratoletsepe.intellectsacademyapi.repositories.CourseRepository;
 import com.leratoletsepe.intellectsacademyapi.repositories.LessonRepository;
 import com.leratoletsepe.intellectsacademyapi.services.interfaces.ILessonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.sql.Date;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class LessonService implements ILessonService {
 
     @Autowired
     LessonRepository lessonRepository;
+    @Autowired
+    CourseRepository courseRepository;
 
     @Override
-    public void AddLesson(Integer courseId, String title, LocalDate date, String content) throws IaBadRequestException {
-        lessonRepository.create(courseId, title, date, content);
+    public void addLesson(Integer courseId, String title, LocalDate date, String content) throws IaBadRequestException {
+        Integer lessonId = lessonRepository.create(courseId, title, date, content);
+        Course course = courseRepository.findById(courseId);
+        List<Lesson> lessons = addLessonToLessonsList(course.getLessons(), lessonId, title, date, content, courseId);
+
+        courseRepository.addLesson(courseId, lessons);
+    }
+
+    private List<Lesson> addLessonToLessonsList(List<Lesson> lessonList, Integer lessonId, String title, LocalDate date, String content, Integer courseId){
+        Lesson lesson = new Lesson(
+                lessonId,
+                title,
+                date,
+                content,
+                courseId
+        );
+        ArrayList<Lesson> les = new ArrayList<>(lessonList);
+        les.add(lesson);
+        return les;
     }
 }
