48001faf6df239b1dd26cb4157476120e1717c2b
lerato1ofone <letsepe100@gmail.com>
2021-05-19 19:54:02 +0200

    Added note service interface with addNote

--- intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/interfaces/INoteService.java
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/interfaces/INoteService.java
@@ -1,4 +1,10 @@
 package com.leratoletsepe.intellectsacademyapi.services.interfaces;
 
-public class INoteService {
+import com.leratoletsepe.intellectsacademyapi.exceptions.IaBadRequestException;
+import com.leratoletsepe.intellectsacademyapi.models.Note;
+
+import java.time.LocalDate;
+
+public interface INoteService {
+    Note addNote(Integer userId, Integer lessonId, Integer courseId, String title, LocalDate date, String content) throws IaBadRequestException;
 }
