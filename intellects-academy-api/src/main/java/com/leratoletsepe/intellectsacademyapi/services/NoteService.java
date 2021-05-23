69bb55f100995ecf5bd4fb30ec6cc511cde28061
lerato1ofone <letsepe100@gmail.com>
2021-05-23 19:54:02 +0200

    Added service implementation for add note

--- intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/NoteService.java
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/NoteService.java
@@ -1,4 +1,25 @@
 package com.leratoletsepe.intellectsacademyapi.services;
 
-public class NoteService {
+import com.leratoletsepe.intellectsacademyapi.exceptions.IaBadRequestException;
+import com.leratoletsepe.intellectsacademyapi.models.Note;
+import com.leratoletsepe.intellectsacademyapi.repositories.NoteRepository;
+import com.leratoletsepe.intellectsacademyapi.services.interfaces.INoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+public class NoteService implements INoteService {
+    @Autowired
+    NoteRepository noteRepository;
+
+    @Override
+    public Note addNote(Integer userId, Integer lessonId, Integer courseId, String title, LocalDate date, String content) throws IaBadRequestException {
+        Note note = noteRepository.create(userId, lessonId, courseId, title, date, content);
+
+        if(note == null)
+            throw new IaBadRequestException("Failed to create note");
+        return note;
+    }
 }
