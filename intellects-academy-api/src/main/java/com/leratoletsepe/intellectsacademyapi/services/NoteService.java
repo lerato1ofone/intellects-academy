2d6571c8c0a356837d35c71967bd9da7b838c43e
lerato1ofone <letsepe100@gmail.com>
2021-05-31 20:15:17 +0200

    Added get by id in note service

--- intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/NoteService.java
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/NoteService.java
@@ -1,6 +1,7 @@
 package com.leratoletsepe.intellectsacademyapi.services;
 
 import com.leratoletsepe.intellectsacademyapi.exceptions.IaBadRequestException;
+import com.leratoletsepe.intellectsacademyapi.exceptions.IaNotFoundException;
 import com.leratoletsepe.intellectsacademyapi.models.Note;
 import com.leratoletsepe.intellectsacademyapi.repositories.NoteRepository;
 import com.leratoletsepe.intellectsacademyapi.services.interfaces.INoteService;
@@ -16,10 +17,20 @@ public class NoteService implements INoteService {
 
     @Override
     public Note addNote(Integer userId, Integer lessonId, Integer courseId, String title, LocalDate date, String content) throws IaBadRequestException {
-        Note note = noteRepository.create(userId, lessonId, courseId, title, date, content);
-
-        if(note == null)
+        Integer noteId = noteRepository.create(userId, lessonId, courseId, title, date, content);
+        if(noteId == null)
             throw new IaBadRequestException("Failed to create note");
+
+        Note note = noteRepository.findById(noteId);
+        return note;
+    }
+
+    @Override
+    public Note getNot(Integer noteId) throws IaNotFoundException {
+        Note note = noteRepository.findById(noteId);
+        if(note == null)
+            throw new IaNotFoundException("Note not found");
+
         return note;
     }
 }
