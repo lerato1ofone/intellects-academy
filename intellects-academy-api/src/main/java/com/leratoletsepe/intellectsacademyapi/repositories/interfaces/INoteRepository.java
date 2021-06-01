cdab74e9a31ed91fb2c7ffad7e21e89b3c00e25d
lerato1ofone <letsepe100@gmail.com>
2021-06-01 12:15:56 +0200

    Added interface function for find by id

--- intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/repositories/interfaces/INoteRepository.java
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/repositories/interfaces/INoteRepository.java
@@ -1,10 +1,13 @@
 package com.leratoletsepe.intellectsacademyapi.repositories.interfaces;
 
 import com.leratoletsepe.intellectsacademyapi.exceptions.IaBadRequestException;
+import com.leratoletsepe.intellectsacademyapi.exceptions.IaNotFoundException;
 import com.leratoletsepe.intellectsacademyapi.models.Note;
 
 import java.time.LocalDate;
 
 public interface INoteRepository {
-    Note create(Integer userId, Integer courseId, Integer lessonId, String title, LocalDate date, String content) throws IaBadRequestException;
+    Integer create(Integer userId, Integer courseId, Integer lessonId, String title, LocalDate date, String content) throws IaBadRequestException;
+
+    Note findById(Integer noteId) throws IaNotFoundException;
 }
