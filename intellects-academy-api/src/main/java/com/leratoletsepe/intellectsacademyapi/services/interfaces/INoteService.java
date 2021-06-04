44d33ca165dce9e3f86187241696d2a63a0c38b2
lerato1ofone <letsepe100@gmail.com>
2021-06-04 18:28:45 +0200

    Added get notes in note service interface

--- intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/interfaces/INoteService.java
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/interfaces/INoteService.java
@@ -5,9 +5,12 @@ import com.leratoletsepe.intellectsacademyapi.exceptions.IaNotFoundException;
 import com.leratoletsepe.intellectsacademyapi.models.Note;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface INoteService {
     Note addNote(Integer userId, Integer lessonId, Integer courseId, String title, LocalDate date, String content) throws IaBadRequestException;
 
     Note getNote(Integer noteId) throws IaNotFoundException;
+
+    List<Note> getNotes(Integer user) throws IaNotFoundException;
 }
