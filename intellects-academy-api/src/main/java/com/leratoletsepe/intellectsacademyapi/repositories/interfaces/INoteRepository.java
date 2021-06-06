935420dec5689cee58fc06675cd53eee7fcc6352
lerato1ofone <letsepe100@gmail.com>
2021-06-06 18:28:45 +0200

    Added get all notes service in repository interface

--- intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/repositories/interfaces/INoteRepository.java
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/repositories/interfaces/INoteRepository.java
@@ -5,9 +5,12 @@ import com.leratoletsepe.intellectsacademyapi.exceptions.IaNotFoundException;
 import com.leratoletsepe.intellectsacademyapi.models.Note;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface INoteRepository {
     Integer create(Integer userId, Integer courseId, Integer lessonId, String title, LocalDate date, String content) throws IaBadRequestException;
 
     Note findById(Integer noteId) throws IaNotFoundException;
+
+    List<Note> findAllForUser(Integer userId) throws IaNotFoundException;
 }
