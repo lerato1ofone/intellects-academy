6a580112c319825c6114acd99ceae8c1b6dac48d
lerato1ofone <letsepe100@gmail.com>
2021-07-09 17:11:14 +0200

    Added user roles enum

---
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/models/dto/enums/UserType.java
@@ -0,0 +1,10 @@
+package com.leratoletsepe.intellectsacademyapi.models.dto.enums;
+
+public class UserType {
+    public enum UserRole {
+        NONE,
+        ADMIN,
+        TEACHER,
+        STUDENT
+    }
+}
