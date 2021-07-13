6a7bf5bd9c410c8f27d454569eead23e2fd9205d
lerato1ofone <letsepe100@gmail.com>
2021-07-13 15:11:52 +0200

    Implement updated and complete userRowMapper for whole user object (for get profile functionality)

--- intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/repositories/UserRepository.java
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/repositories/UserRepository.java
@@ -1,16 +1,13 @@
 package com.leratoletsepe.intellectsacademyapi.repositories;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
+import com.google.gson.Gson;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ser.std.StdKeySerializers;
 import com.leratoletsepe.intellectsacademyapi.exceptions.IaBadRequestException;
+import com.leratoletsepe.intellectsacademyapi.exceptions.IaNotFoundException;
 import com.leratoletsepe.intellectsacademyapi.models.Course;
 import com.leratoletsepe.intellectsacademyapi.models.Note;
 import com.leratoletsepe.intellectsacademyapi.models.dto.User;
-import com.leratoletsepe.intellectsacademyapi.models.dto.enums.UserType;
 import com.leratoletsepe.intellectsacademyapi.models.dto.enums.UserType.UserRole;
-import org.apache.commons.lang3.text.WordUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -21,6 +18,8 @@ import org.springframework.stereotype.Repository;
 
 import java.sql.PreparedStatement;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Repository
@@ -32,6 +31,10 @@ public class UserRepository implements com.leratoletsepe.intellectsacademyapi.re
     private static final String SQL_COUNT_BY_EMAIL = "SELECT COUNT(*) FROM IA_USERS WHERE EMAIL = ?";
 
     private static final String SQL_FIND_BY_ID = "SELECT USER_ID, TITLE, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, ROLE FROM IA_USERS WHERE USER_ID = ?";
+
+    private static final String SQL_FINDWHOLEUSER_BY_ID = "SELECT USER_ID, TITLE, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, ROLE, DOB, SEMESTER_MARK, OFFICE_NUMBER, AVATAR, NOTES, COURSES  FROM IA_USERS WHERE USER_ID = ?";
+
+    private static final String SQL_FIND_BY_EMAIL_PASSWORD = "SELECT USER_ID, TITLE, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, ROLE FROM IA_USERS WHERE EMAIL = ? AND PASSWORD = ?";
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
@@ -62,8 +65,13 @@ public class UserRepository implements com.leratoletsepe.intellectsacademyapi.re
     }
 
     @Override
-    public User findByEmailAndPassword(String email, String password) {
-        return null;
+    public User findByEmailAndPassword(String email, String password) throws IaBadRequestException {
+        try{
+            return jdbcTemplate.queryForObject(SQL_FIND_BY_EMAIL_PASSWORD, new Object[]{email, password}, userDtoRowMapper);
+        }
+        catch(EmptyResultDataAccessException ex){
+            throw new IaBadRequestException("Invalid credentials.");
+        }
     }
 
     @Override
@@ -72,12 +80,21 @@ public class UserRepository implements com.leratoletsepe.intellectsacademyapi.re
     }
 
     @Override
-    public User findById(Integer userId) {
+    public User findById(Integer userId) throws IaNotFoundException {
         try{
             return jdbcTemplate.queryForObject(SQL_FIND_BY_ID, new Object[]{userId}, userDtoRowMapper);
         }
         catch(EmptyResultDataAccessException ex){
-            return null;
+            throw new IaNotFoundException("User with id " + userId + " not found");
+        }
+    }
+
+    public com.leratoletsepe.intellectsacademyapi.models.User getUserProfileById(Integer userId) throws IaNotFoundException{
+        try{
+            return jdbcTemplate.queryForObject(SQL_FINDWHOLEUSER_BY_ID, new Object[]{ userId }, userRowMapper);
+        }
+        catch(EmptyResultDataAccessException ex){
+            throw new IaNotFoundException("User with id " + userId + " not found");
         }
     }
 
@@ -93,26 +110,30 @@ public class UserRepository implements com.leratoletsepe.intellectsacademyapi.re
     });
 
     private RowMapper<com.leratoletsepe.intellectsacademyapi.models.User> userRowMapper = ((rs, rowNumber) -> {
-        System.out.println( rs.getString("DOB"));
-        try {
-            return new com.leratoletsepe.intellectsacademyapi.models.User(
-                    rs.getInt("USER_ID"),
-                    rs.getString("TITLE"),
-                    rs.getString("FIRST_NAME"),
-                    rs.getString("LAST_NAME"),
-                    rs.getString("EMAIL"),
-                    rs.getString("PASSWORD"),
-                    UserRole.valueOf(rs.getString("ROLE")),
-                    rs.getDate("DOB"),
-                    rs.getDouble("SEMESTER_MARK"),
-                    rs.getString("OFFICE_NUMBER"),
-                    rs.getBytes("AVATAR"),
-                    objectMapper.readValue(rs.getString("NOTES"), new TypeReference<List<Note>>(){}),
-                    objectMapper.readValue(rs.getString("COURSES"), new TypeReference<List<Course>>(){})
-                    );
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-            return null;
-        }
+
+        var notesResultSet = rs.getString("NOTES");
+        var coursesResultSet = rs.getString("COURSES");
+
+        List<Note> notes = notesResultSet == null ? new ArrayList<>() :
+                Arrays.asList(new Gson().fromJson(notesResultSet, Note[].class));
+
+        List<Course> courses = coursesResultSet == null ? new ArrayList<>() :
+                Arrays.asList(new Gson().fromJson(coursesResultSet, Course[].class));
+
+        return new com.leratoletsepe.intellectsacademyapi.models.User(
+                rs.getInt("USER_ID"),
+                rs.getString("TITLE"),
+                rs.getString("FIRST_NAME"),
+                rs.getString("LAST_NAME"),
+                rs.getString("EMAIL"),
+                rs.getString("PASSWORD"),
+                UserRole.valueOf(rs.getString("ROLE")),
+                rs.getDate("DOB"),
+                rs.getDouble("SEMESTER_MARK"),
+                rs.getString("OFFICE_NUMBER"),
+                rs.getBytes("AVATAR"),
+                notes,
+                courses
+                );
     });
 }
