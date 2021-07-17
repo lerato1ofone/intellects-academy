3cc94689161b2e589e3964837567e423a9aa323d
lerato1ofone <letsepe100@gmail.com>
2021-07-17 21:02:01 +0200

    Updated return types of register user and login to UserDto

--- intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/interfaces/UserService.java
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/interfaces/UserService.java
@@ -1,13 +1,11 @@
 package com.leratoletsepe.intellectsacademyapi.services.interfaces;
 
 import com.leratoletsepe.intellectsacademyapi.exceptions.IaBadRequestException;
-import com.leratoletsepe.intellectsacademyapi.exceptions.IaNotFoundException;
-import com.leratoletsepe.intellectsacademyapi.models.dto.User;
-import com.leratoletsepe.intellectsacademyapi.models.dto.enums.UserType;
+import com.leratoletsepe.intellectsacademyapi.models.dto.UserDto;
 
 public interface UserService {
 
-    User registerUser(String title, String firstName, String lastName, String email, String password, String role) throws IaBadRequestException;
+    UserDto registerUser(String title, String firstName, String lastName, String email, String password, String role) throws IaBadRequestException;
 
-    User validateUser(String email, String password) throws IaBadRequestException;
+    UserDto validateUser(String email, String password) throws IaBadRequestException;
 }
