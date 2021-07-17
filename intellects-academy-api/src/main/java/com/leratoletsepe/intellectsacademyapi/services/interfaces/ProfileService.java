7530e1080012dac6d1e6749846da43b644afb45f
lerato1ofone <letsepe100@gmail.com>
2021-07-17 20:55:32 +0200

    Updated return type of update user to UserProfileDto response

--- intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/interfaces/ProfileService.java
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/interfaces/ProfileService.java
@@ -1,9 +1,12 @@
 package com.leratoletsepe.intellectsacademyapi.services.interfaces;
 
+import com.leratoletsepe.intellectsacademyapi.exceptions.IaBadRequestException;
 import com.leratoletsepe.intellectsacademyapi.exceptions.IaNotFoundException;
 import com.leratoletsepe.intellectsacademyapi.models.User;
+import com.leratoletsepe.intellectsacademyapi.models.dto.UserProfileDto;
 
 public interface ProfileService {
+    UserProfileDto getUserProfileById(Integer userId) throws IaNotFoundException;
 
-    User getUserProfileById(Integer userId) throws IaNotFoundException;
+    UserProfileDto updateProfile(Integer userId, User user) throws IaBadRequestException;
 }
