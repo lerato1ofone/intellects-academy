8937c972ca86fb9200eb5a24db565121e42150e4
lerato1ofone <letsepe100@gmail.com>
2021-07-18 20:12:00 +0200

    Added course service inteface

--- intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/interfaces/ICourseService.java
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/interfaces/ICourseService.java
@@ -1,4 +1,8 @@
 package com.leratoletsepe.intellectsacademyapi.services.interfaces;
 
+import com.leratoletsepe.intellectsacademyapi.exceptions.IaBadRequestException;
+import com.leratoletsepe.intellectsacademyapi.models.Course;
+
 public interface ICourseService {
+    void addCourse(Integer userId, Course course) throws IaBadRequestException;
 }
