0f157fa5649194cc277e088747655fd0b623f359
lerato1ofone <letsepe100@gmail.com>
2021-07-18 20:12:18 +0200

    Add endpoint to add a course (@TODO: implement service and repository)

--- intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/controllers/CourseController.java
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/controllers/CourseController.java
@@ -1,9 +1,34 @@
 package com.leratoletsepe.intellectsacademyapi.controllers;
 
+import com.leratoletsepe.intellectsacademyapi.models.Course;
+import com.leratoletsepe.intellectsacademyapi.services.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/course")
 public class CourseController {
+
+    @Autowired
+    CourseService courseService;
+
+    @PostMapping("")
+    public ResponseEntity<Map<String, Boolean>> addCourse(HttpServletRequest request,
+                                                    @RequestBody Course course){
+        Integer userId = (Integer) request.getAttribute("userId");
+        courseService.addCourse(userId, course);
+
+        Map<String, Boolean> map = new HashMap<>();
+        map.put("success", true);
+        return new ResponseEntity<>(map, HttpStatus.OK);
+    }
 }
---
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/repositories/interfaces/ICourseRepository.java
@@ -0,0 +1,4 @@
+package com.leratoletsepe.intellectsacademyapi.repositories.interfaces;
+
+public class ICourseRepository {
+}
