c53198c6e12fbd9d7a77fd31221fdcf13485f5e1
lerato1ofone <letsepe100@gmail.com>
2021-07-18 20:11:28 +0200

    Added course repository interface

--- intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/repositories/interfaces/ICourseRepository.java
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/repositories/interfaces/ICourseRepository.java
@@ -1,4 +1,10 @@
 package com.leratoletsepe.intellectsacademyapi.repositories.interfaces;
 
-public class ICourseRepository {
+import com.leratoletsepe.intellectsacademyapi.exceptions.IaBadRequestException;
+import com.leratoletsepe.intellectsacademyapi.models.Lesson;
+
+import java.util.List;
+
+public interface ICourseRepository {
+    void create(Integer userId, Integer courseId, String title, String description, List<Lesson> lessons) throws IaBadRequestException;
 }
