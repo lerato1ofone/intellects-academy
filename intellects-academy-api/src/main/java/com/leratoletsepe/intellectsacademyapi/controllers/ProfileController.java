aeaa711b6986b4b74ec5b6b91ea7c831889fcf11
lerato1ofone <letsepe100@gmail.com>
2021-07-19 19:58:52 +0200

    Added update user profile endpoint in controller.

--- intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/controllers/ProfileController.java
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/controllers/ProfileController.java
@@ -5,10 +5,7 @@ import com.leratoletsepe.intellectsacademyapi.services.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -24,5 +21,15 @@ public class ProfileController {
         User user = profileService.getUserProfileById(userId);
 
         return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @PutMapping("")
+    public ResponseEntity<User> updateUserProfile(HttpServletRequest request,
+                                           @RequestBody User user){
+        Integer userId = (Integer) request.getAttribute("userId");
+
+        User updatedUser = profileService.updateProfile(userId, user);
+
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
     }
 }
