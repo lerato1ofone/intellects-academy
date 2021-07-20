b32e09697d02f0f96640f96905b158e0f175c11b
lerato1ofone <letsepe100@gmail.com>
2021-07-20 21:02:50 +0200

    Added course service, and implementation for addCourse

--- intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/CourseService.java
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/CourseService.java
@@ -1,4 +1,19 @@
 package com.leratoletsepe.intellectsacademyapi.services;
 
-public class CourseService {
+import com.leratoletsepe.intellectsacademyapi.exceptions.IaBadRequestException;
+import com.leratoletsepe.intellectsacademyapi.models.Course;
+import com.leratoletsepe.intellectsacademyapi.repositories.CourseRepository;
+import com.leratoletsepe.intellectsacademyapi.services.interfaces.ICourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseService implements ICourseService {
+    @Autowired
+    CourseRepository courseRepository;
+
+    @Override
+    public void addCourse(Integer userId, Course course) throws IaBadRequestException {
+        courseRepository.create(userId, course.getCourseId(), course.getTitle(), course.getDescription(), course.getLessons());
+    }
 }
