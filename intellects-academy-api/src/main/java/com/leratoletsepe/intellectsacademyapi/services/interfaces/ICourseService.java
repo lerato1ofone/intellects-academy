627284a5bc6828c1852e1d7a8cb89f146c5ee129
lerato1ofone <letsepe100@gmail.com>
2021-07-21 15:45:06 +0200

    Updated course service interface parameters

--- intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/interfaces/ICourseService.java
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/interfaces/ICourseService.java
@@ -1,8 +1,10 @@
 package com.leratoletsepe.intellectsacademyapi.services.interfaces;
 
 import com.leratoletsepe.intellectsacademyapi.exceptions.IaBadRequestException;
-import com.leratoletsepe.intellectsacademyapi.models.Course;
+import com.leratoletsepe.intellectsacademyapi.models.Lesson;
+
+import java.util.List;
 
 public interface ICourseService {
-    void addCourse(Integer userId, Course course) throws IaBadRequestException;
+    void addCourse(Integer userId, String title, String description, List<Lesson> lessons) throws IaBadRequestException;
 }
