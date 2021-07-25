a91921760604ee6df71a547a0c7d8a0a524c2ad7
lerato1ofone <letsepe100@gmail.com>
2021-07-25 23:04:52 +0200

    Updated params for get lessons in lesson interface

--- intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/interfaces/ILessonService.java
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/interfaces/ILessonService.java
@@ -2,11 +2,15 @@ package com.leratoletsepe.intellectsacademyapi.services.interfaces;
 
 import com.leratoletsepe.intellectsacademyapi.exceptions.IaBadRequestException;
 import com.leratoletsepe.intellectsacademyapi.exceptions.IaNotFoundException;
+import com.leratoletsepe.intellectsacademyapi.models.Lesson;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface ILessonService {
     void addLesson(Integer courseId, String title, LocalDate date, String content) throws IaBadRequestException;
 
     void deleteLesson(Integer userId, Integer lessonId) throws IaNotFoundException;
+
+    List<Lesson> getLessons(Integer courseId) throws IaNotFoundException;
 }
