ad6d25fb03bc66fb05f84a5394020287cc4ec92d
lerato1ofone <letsepe100@gmail.com>
2021-07-27 13:53:57 +0200

    Completed assessment service to add and get assessment

--- intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/AssessmentService.java
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/AssessmentService.java
@@ -1,4 +1,37 @@
 package com.leratoletsepe.intellectsacademyapi.services;
 
-public class AssessmentService {
+import com.leratoletsepe.intellectsacademyapi.exceptions.IaBadRequestException;
+import com.leratoletsepe.intellectsacademyapi.exceptions.IaNotFoundException;
+import com.leratoletsepe.intellectsacademyapi.models.Assessment;
+import com.leratoletsepe.intellectsacademyapi.repositories.AssessmentRepository;
+import com.leratoletsepe.intellectsacademyapi.services.interfaces.IAssessmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+public class AssessmentService implements IAssessmentService {
+
+    @Autowired
+    AssessmentRepository assessmentRepository;
+
+    @Override
+    public Assessment addAssessment(String title, LocalDate date, Integer courseId) throws IaBadRequestException {
+        Integer assessmentId = assessmentRepository.create(title, date, courseId);
+        if(assessmentId == null)
+            throw new IaBadRequestException("Failed to create assessment");
+
+        Assessment assessment = this.getAssessment(assessmentId);
+        return assessment;
+    }
+
+    @Override
+    public Assessment getAssessment(Integer assessmentId) throws IaNotFoundException {
+        Assessment assessment = assessmentRepository.findById(assessmentId);
+        if(assessment == null)
+            throw new IaBadRequestException("Failed to get assessment");
+
+        return assessment;
+    }
 }
