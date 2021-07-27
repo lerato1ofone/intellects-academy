3e25256f39ecf34af513061567fba05b8ed3c772
lerato1ofone <letsepe100@gmail.com>
2021-07-27 13:48:12 +0200

    Added assessment find by id in repository interface

--- intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/repositories/interfaces/IAssessmentRepository.java
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/repositories/interfaces/IAssessmentRepository.java
@@ -1,9 +1,13 @@
 package com.leratoletsepe.intellectsacademyapi.repositories.interfaces;
 
 import com.leratoletsepe.intellectsacademyapi.exceptions.IaBadRequestException;
+import com.leratoletsepe.intellectsacademyapi.exceptions.IaNotFoundException;
+import com.leratoletsepe.intellectsacademyapi.models.Assessment;
 
 import java.time.LocalDate;
 
 public interface IAssessmentRepository {
     Integer create(String title, LocalDate date, Integer courseId) throws IaBadRequestException;
+
+    Assessment findById(String assessmentId) throws IaNotFoundException;
 }
