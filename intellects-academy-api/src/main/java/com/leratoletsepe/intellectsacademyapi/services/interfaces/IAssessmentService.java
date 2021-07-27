e034cd6a2d486a6a68b0108ad715f1dbd0939de0
lerato1ofone <letsepe100@gmail.com>
2021-07-27 13:54:27 +0200

    Added a get assessment function in assessment service interface

--- intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/interfaces/IAssessmentService.java
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/interfaces/IAssessmentService.java
@@ -1,10 +1,13 @@
 package com.leratoletsepe.intellectsacademyapi.services.interfaces;
 
 import com.leratoletsepe.intellectsacademyapi.exceptions.IaBadRequestException;
+import com.leratoletsepe.intellectsacademyapi.exceptions.IaNotFoundException;
 import com.leratoletsepe.intellectsacademyapi.models.Assessment;
 
 import java.time.LocalDate;
 
 public interface IAssessmentService {
     Assessment addAssessment(String title, LocalDate date, Integer courseId) throws IaBadRequestException;
+
+    Assessment getAssessment(Integer assessmentId) throws IaNotFoundException;
 }
