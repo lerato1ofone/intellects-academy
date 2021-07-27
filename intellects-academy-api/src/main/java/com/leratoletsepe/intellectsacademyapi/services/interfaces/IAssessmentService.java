b351f63c10425c9004f9b48f3068f82c29a5d968
lerato1ofone <letsepe100@gmail.com>
2021-07-27 13:33:32 +0200

    Added add assessment in service interface

--- intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/interfaces/IAssessmentService.java
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/services/interfaces/IAssessmentService.java
@@ -1,4 +1,10 @@
 package com.leratoletsepe.intellectsacademyapi.services.interfaces;
 
+import com.leratoletsepe.intellectsacademyapi.exceptions.IaBadRequestException;
+import com.leratoletsepe.intellectsacademyapi.models.Assessment;
+
+import java.time.LocalDate;
+
 public interface IAssessmentService {
+    Assessment addAssessment(String title, LocalDate date, Integer courseId) throws IaBadRequestException;
 }
