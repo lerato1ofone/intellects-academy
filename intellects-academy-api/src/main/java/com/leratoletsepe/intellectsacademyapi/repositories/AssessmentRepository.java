eb9062f3b3ac46ecb748c1aae6210af00d1faf34
lerato1ofone <letsepe100@gmail.com>
2021-07-27 13:39:17 +0200

    Added base for assessment repository (@TODO: implement create function and SQL)

--- intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/repositories/AssessmentRepository.java
+++ intellects-academy-api/src/main/java/com/leratoletsepe/intellectsacademyapi/repositories/AssessmentRepository.java
@@ -1,4 +1,25 @@
 package com.leratoletsepe.intellectsacademyapi.repositories;
 
-public class AssessmentRepository {
+import com.leratoletsepe.intellectsacademyapi.exceptions.IaBadRequestException;
+import com.leratoletsepe.intellectsacademyapi.repositories.interfaces.IAssessmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+
+@Repository
+public class AssessmentRepository implements IAssessmentRepository {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public Integer create(String title, LocalDate date, Integer courseId) throws IaBadRequestException {
+       try {
+
+       } catch (Exception e) {
+           throw new IaBadRequestException("Failed to create assessment, try again later.");
+       }
+    }
 }
